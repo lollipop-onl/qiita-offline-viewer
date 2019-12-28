dd913a5b79435d03dfe0b1e1d3a8a8bbe21efbbc
lollipop-onl <lollipop@simoochee.net>
2019-12-28 12:09:55 +0900

    🎉 initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { Configuration } from '@nuxt/types'
+
+const config: Configuration = {
+  mode: 'universal',
+  modern: 'client',
+  srcDir: 'src'
+}
+
+module.exports = config
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is a sample project</h1>
+    <p>Welcome to the sample project page!</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class IndexPage extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
